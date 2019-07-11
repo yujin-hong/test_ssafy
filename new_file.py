5a70223bef61c6768b81ea38aeb4adad3f702b13
Yujin Hong <dbwlsdl2500@gist.ac.kr>
2019-07-12 00:07:54 +0900

    resolve conflict

--- new_file.py
+++ new_file.py
@@ -1,8 +1,5 @@
 print('hello git!')
 print('I add one more line :\)')
 print('this is third in add_button_for_yuj')
-<<<<<<< HEAD
 print('modify in master')
-=======
 print('modify in new branch')
->>>>>>> add_button_for_yuj
